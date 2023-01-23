c298d6eed25c3b828a5f7773b6243e6c57c233a0
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-23 06:07:53 +0100

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Philipp Sanktjohanser on 23.01.23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
